3011b43a1dab6e1e35b63ce032b1e91eaf882c59
harsh <animeart711@gmail.com>
2026-04-19 14:38:54 +0530

    new

--- lib/features/products/data/product_api.dart
+++ lib/features/products/data/product_api.dart
@@ -5,13 +5,22 @@ import '../model/products.dart';
 class ProductApi {
   Future<List<Product>> fetchProducts() async {
     final response = await http.get(
-      Uri.parse("https://shopit-flutter-app.onrender.com"), 
+      Uri.parse("https://shopit-flutter-app.onrender.com"),
     );
 
     if (response.statusCode == 200) {
-      final List data = jsonDecode(response.body); 
+      final decoded = jsonDecode(response.body);
 
-      return data.map((e) => Product.fromJson(e)).toList();
+      // 🔥 HANDLE BOTH MAP & LIST
+      if (decoded is List) {
+        return decoded.map((e) => Product.fromJson(e)).toList();
+      } else if (decoded is Map && decoded['products'] is List) {
+        return (decoded['products'] as List)
+            .map((e) => Product.fromJson(e))
+            .toList();
+      } else {
+        throw Exception("Invalid API format");
+      }
     } else {
       throw Exception("Failed to load products");
     }
