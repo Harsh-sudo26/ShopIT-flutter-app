e6c2fc344988c7de844c633b9a0e4f1d65a901e5
harsh <animeart711@gmail.com>
2026-04-23 17:52:50 +0530

    SPLsh updATED

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
 import 'package:shopit/features/products/model/auth_viewmodel.dart';
 import 'package:shopit/features/products/model/cartview_model.dart';
 import 'package:shopit/features/products/model/favoriteview_model.dart';
 import 'package:shopit/features/products/model/productview_moderl.dart';
+
 import 'package:shopit/features/products/repository/product_repo.dart';
 import 'package:shopit/features/products/service/product_api.dart';
-import 'package:shopit/screens/splash.dart';
+
+import 'package:shopit/screens/Auth/Authscreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,26 +22,26 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+       
         Provider(create: (_) => ProductApi()),
 
+       
         Provider(
           create: (context) =>
               ProductRepository(api: context.read<ProductApi>()),
         ),
 
-        // 🔹 ViewModels
+      
         ChangeNotifierProvider(create: (_) => FavoriteViewModel()),
         ChangeNotifierProvider(create: (_) => CartViewModel()),
+
+        
         ChangeNotifierProvider(
           create: (context) =>
-              ProductViewModel(context.read<ProductRepository>())
-                ..fetchProducts(),
+              ProductViewModel(context.read<ProductRepository>()),
         ),
 
-        // ✅ FIXED (NO child here)
-        ChangeNotifierProvider(
-          create: (_) => AuthViewModel(),
-        ),
+        ChangeNotifierProvider(create: (_) => AuthViewModel()),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
@@ -47,7 +50,8 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
           useMaterial3: true,
         ),
-        home: const SplashScreen(),
+
+        home: const AuthScreen(),
       ),
     );
   }
