8f9dbfe8114e8df484103031eb0229f393c12524
harsh <animeart711@gmail.com>
2026-04-23 17:18:59 +0530

    update

--- lib/features/products/service/product_api.dart
+++ lib/features/products/service/product_api.dart
@@ -7,7 +7,7 @@ class ProductApi {
   Future<List<Product>> fetchProducts() async {
     try {
       final response = await http
-          .get(Uri.parse("http://192.168.1.11:8000/products/"))
+          .get(Uri.parse("https://shopit-flutter-app.onrender.com/products"))
           .timeout(const Duration(seconds: 30));
 
       print("STATUS: ${response.statusCode}");
