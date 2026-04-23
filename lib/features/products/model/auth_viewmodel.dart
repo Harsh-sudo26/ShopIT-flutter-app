e14b42ea30f93f224b65603824e943f8a21fc1b5
harsh <animeart711@gmail.com>
2026-04-23 14:46:02 +0530

    update

--- lib/features/products/model/auth_viewmodel.dart
+++ lib/features/products/model/auth_viewmodel.dart
@@ -2,11 +2,43 @@ import 'package:flutter/material.dart';
 
 class AuthViewModel extends ChangeNotifier {
   int _selectedIndex = 0;
-
   int get selectedIndex => _selectedIndex;
 
+  bool _isLoading = false;
+  bool get isLoading => _isLoading;
+
+  // 🔹 Tab switch
   void switchTab(int index) {
     _selectedIndex = index;
+    notifyListeners();
+  }
+
+ 
+  Future<void> login(
+    String email,
+    String password,
+    BuildContext context,
+  ) async {
+    _isLoading = true;
+    notifyListeners();
+
+    try {
+    
+      await Future.delayed(const Duration(seconds: 2));
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Login Successful")),
+      );
+
+     
+      switchTab(0);
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Login Failed")),
+      );
+    }
+
+    _isLoading = false;
     notifyListeners();
   }
 }
--- lib/screens/Auth/login.dart
+++ lib/screens/Auth/login.dart
@@ -1,16 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopit/features/products/model/auth_viewmodel.dart';
+import 'package:shopit/features/products/view/home.dart';
+import 'package:shopit/screens/Auth/Authscreen.dart';
+import 'package:shopit/widget/%20button.dart';
+import 'package:shopit/widget/textfieldwidget.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final authViewModel = Provider.of<AuthViewModel>(context);
+    TextEditingController email = TextEditingController();
+    TextEditingController password = TextEditingController();
     return Scaffold(
-      body: Column(
-        children: const [
-          Text("Login Page"),
-          // your textfields here
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Welcome Back!",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "Access your curated selection.",
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+            Textfield(
+              icon: const Icon(Icons.email),
+              hinttext: 'Enter Email',
+              rowtext: '',
+              controller: email,
+            ),
+            Textfield(
+              icon: const Icon(Icons.password),
+              hinttext: 'Enter Pasword',
+              rowtext: '',
+              controller: email,
+            ),
+            SizedBox(height: 100),
+            authViewModel.isLoading
+                ? const CircularProgressIndicator()
+                : buttonsim(
+                    simplebtncolor: Colors.blue,
+                    simplebuttontext: const Text(
+                      "Login",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    onPressed: () async {
+                      authViewModel.login(
+                        email.text.trim(),
+                        password.text.trim(),
+                        await Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => homepage()),
+                        ),
+                      );
+                    },
+                  ),
+          ],
+        ),
       ),
     );
   }
